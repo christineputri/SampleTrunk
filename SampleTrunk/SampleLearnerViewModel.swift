08fd3e9a2ff5e657676a6b4fa671e3108264fe19
christineputri <christine.putri@binus.ac.id>
2024-07-12 10:13:20 +0700

    commit lagi

--- SampleTrunk/SampleLearnerViewModel.swift
+++ SampleTrunk/SampleLearnerViewModel.swift
@@ -1,8 +1,16 @@
 //
-//  SampleLearnerViewModel.swift
+//  File.swift
 //  SampleTrunk
 //
 //  Created by Christine Putri on 12/07/24.
 //
 
 import Foundation
+class SampleLearnerViewModel: LearnerDatabaseProtocol{
+    func getLearnerBy(id: Int) -> Bool{
+        return true
+    }
+    func editLearnerBy(id:Int) -> Bool{
+        return true
+    }
+}
