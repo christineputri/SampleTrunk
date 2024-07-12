ab1b039445a63fd3f03cbac24e2aa5deb9d97d39
christineputri <christine.putri@binus.ac.id>
2024-07-12 09:52:53 +0700

    Initial Commit

---
+++ SampleTrunk/SampleTrunkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTrunkApp.swift
+//  SampleTrunk
+//
+//  Created by Christine Putri on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleTrunkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
